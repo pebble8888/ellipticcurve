96a2c542321c4f1e5d49c3ff87f189a83d91e4c8
pebble8888 <pebble8888@gmail.com>
2019-07-09 00:02:09 +0900

    x,y polynomial

--- src/unit.rs
+++ src/unit.rs
@@ -3,224 +3,169 @@ extern crate num_traits;
 
 use num_bigint::BigInt;
 use num_traits::One;
+use num_traits::Zero;
 use std::cmp::Ordering;
 use std::ops::Add;
 
 #[derive(Debug, Clone)]
-struct Polynomial<'a> {
-    units: Vec<&'a Unit<'a>>,
+struct Polynomial {
+    units: Vec<Unit>,
 }
 
-// 3x^4y^5
-// coef: 3
-// variables: [x^4, y^5]
-#[derive(Debug, Clone)]
-struct Unit<'a> {
-    coef: BigInt,
-    variables: Vec<&'a Variable<'a>>, // always sorted
-}
-
-// x^4
-// power: 4
-// name: x
+// 3 x^4 y^5
 #[derive(Debug, Clone, PartialEq, Eq)]
-struct Variable<'a> {
-    power: BigInt,
-    name: &'a str,
+struct Unit {
+    coef: BigInt, // 3
+    xpow: BigInt, // 4
+    ypow: BigInt, // 5
 }
 
 use std::fmt;
 
 // Unit
 
-/*
-impl<'a> Add for Unit<'a> {
-    type Output = Unit
-    fn add(self, other: Output) -> Output {
-
-    }
-}
-*/
-
-impl<'a> PartialEq for Unit<'a> {
-    fn eq(&self, other: &Self) -> bool {
-        let mut s = self.clone();
+impl Add for Polynomial {
+    type Output = Polynomial;
+    fn add(self, other: Self) -> Self {
+        let mut units = self.units;
+        let mut other_units = other.units;
+        units.append(&mut other_units);
+        let mut s = Self { units: units };
         s.normalize();
-        let mut o = other.clone();
-        o.normalize();
-        s.coef == o.coef && s.variables == s.variables
+        s
     }
 }
 
-// TODO:
-/*
-impl<'a> Ord for Unit<'a> {
+impl Unit {
+    pub fn equal_order(&self, other: &Self) -> bool {
+        return &self.xpow == &other.xpow && self.ypow == other.ypow 
+    }
+}
+
+impl Ord for Unit {
     fn cmp(&self, other: &Self) -> Ordering {
-    }
-}
-*/
-
-impl<'a> fmt::Display for Polynomial<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut s = self.clone();
-        s.normalize();
-        let mut st = String::new();
-        for i in &s.units {
-            st.push_str(&i.to_string());
-            st.push_str(" ");
+        if self.xpow < other.xpow {
+            return Ordering::Less;
+        } else if self.xpow > other.xpow {
+            return Ordering::Greater;
         }
-        write!(f, "{}", st.trim_end())
-    }
-}
-
-impl<'a> Polynomial<'a> {
-    pub fn normalize(&mut self) {
-        self.units.sort();
-    }
-}
-
-impl<'a> Unit<'a> {
-    pub fn normalize(&mut self) {
-        self.variables.sort();
-    }
-}
-
-impl<'a> fmt::Display for Unit<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut s = self.clone();
-        s.normalize();
-        if s.coef == One::one() {
-            let mut st = String::new();
-            for i in &s.variables {
-                st.push_str(&i.to_string());
-                st.push_str(" ");
-            }
-            write!(f, "{}", st.trim_end())
-        } else {
-            let mut st = String::new();
-            st.push_str(&s.coef.to_string());
-            st.push_str(" ");
-            for i in &s.variables {
-                st.push_str(&i.to_string());
-                st.push_str(" ");
-            }
-            write!(f, "{}", st.trim_end())
+        if self.ypow < other.ypow {
+            return Ordering::Less;
+        } else if self.ypow > other.ypow {
+            return Ordering::Greater;
         }
+        return Ordering::Equal;
     }
 }
 
-// Variable
-
-impl<'a> PartialOrd for Variable<'a> {
+impl PartialOrd for Unit {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
 }
 
-impl<'a> Ord for Variable<'a> {
-    fn cmp(&self, other: &Self) -> Ordering {
-        if self.name < other.name {
-            Ordering::Less
-        } 
-        else if self.name > other.name {
-            Ordering::Greater
-        }
-        else if self.power < other.power {
-            Ordering::Less
-        }
-        else if self.power > other.power {
-            Ordering::Greater
-        }
-        else { 
-            Ordering::Equal
-        }
-    }
-}
-
-impl<'a> fmt::Display for Variable<'a> {
+impl fmt::Display for Polynomial {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if self.power == One::one() { 
-            write!(f, "{}", self.name)
-        } else {
-            write!(f, "{}^{}", self.name, self.power)
+        let mut s = self.clone();
+        s.normalize();
+        let mut st = String::new();
+        let mut i = 0;
+        for u in &s.units {
+            if u.coef > Zero::zero() && i != 0 {
+                st.push_str("+ ");
+            }
+            st.push_str(&u.to_string());
+            st.push_str(" ");
+            i = i + 1;
+        }
+        write!(f, "{}", st.trim_end())
+    }
+}
+
+impl Polynomial {
+    pub fn normalize(&mut self) {
+        let units = &mut self.units;
+        units.sort_by(|a, b| b.cmp(a));
+        let mut i = 0; 
+        while i+1 < units.len() {
+            if units[i].equal_order(&units[i+1]) {
+                let coef = units[i].coef.clone() + units[i+1].coef.clone();
+                let mut unit = units[i].clone();
+                unit.coef = coef;
+                units.insert(i, unit);
+                units.remove(i+1);
+                units.remove(i+1);
+            }
+            i = i + 1;
         }
     }
 }
 
-// test variable
-
-#[test]
-fn variable() {
-    let a1 = Variable {
-        power: One::one(),
-        name: "a",
-    };
-    let a2 = Variable {
-        power: BigInt::from(2),
-        name: "a",
-    };
-    let b1 = Variable {
-        power: One::one(),
-        name: "b",
-    };
-    let b2 = Variable {
-        power: BigInt::from(2),
-        name: "b",
-    };
-    let a31 = Variable {
-        power: BigInt::from(31),
-        name: "a",
-    };
-    let ba3 = Variable {
-        power: BigInt::from(3),
-        name: "ba",
-    };
-    assert_eq!(a1.to_string(), "a");
-    assert_eq!(a2.to_string(), "a^2");
-    assert_eq!(b1.to_string(), "b");
-    assert_eq!(b2.to_string(), "b^2");
-    assert_eq!(a31.to_string(), "a^31");
-    assert_eq!(ba3.to_string(), "ba^3");
-
-    assert!(a1 < a2);
-    assert!(a1 < b1);
-    assert!(b1 < b2);
+impl fmt::Display for Unit {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.coef == One::one() {
+            if self.xpow == One::one() && self.ypow == One::one() {
+                write!(f, "1")
+            } else {
+                let mut st = String::new();
+                if self.xpow != One::one() {
+                    st.push_str("x^");
+                    st.push_str(&self.xpow.to_string());
+                    st.push_str(" ");
+                }
+                if self.ypow != One::one() {
+                    st.push_str("y^");
+                    st.push_str(&self.ypow.to_string());
+                }
+                write!(f, "{}", st.trim_end())
+            }
+        } else {
+            let mut st = String::new();
+            st.push_str(&self.coef.to_string());
+            st.push_str(" ");
+            if self.xpow != One::one() {
+                st.push_str("x^");
+                st.push_str(&self.xpow.to_string());
+                st.push_str(" ");
+            }
+            if self.ypow != One::one() {
+                st.push_str("y^");
+                st.push_str(&self.ypow.to_string());
+            }
+            write!(f, "{}", st.trim_end())
+        }
+    }
 }
 
 // test unit
 
 #[test]
 fn unit() {
-    let a2 = Variable {
-        power: BigInt::from(2),
-        name: "a",
-    };
-    let b2 = Variable {
-        power: BigInt::from(2),
-        name: "b",
-    };
     let u1 = Unit {
         coef: One::one(),
-        variables: vec![&a2, &b2],
+        xpow: BigInt::from(4),
+        ypow: BigInt::from(2),
     };
     let u2 = Unit {
-        coef: One::one(),
-        variables: vec![&b2, &a2],
-    };
-    assert_eq!(u1.to_string(), "a^2 b^2");
-    assert_eq!(u2.to_string(), "a^2 b^2");
-
-    let u3_1 = Unit {
         coef: BigInt::from(3),
-        variables: vec![&a2, &b2],
+        xpow: BigInt::from(2),
+        ypow: BigInt::from(4),
     };
-    let u5_2 = Unit {
-        coef: BigInt::from(5),
-        variables: vec![&b2, &a2],
-    };
-    assert_eq!(u3_1.to_string(), "3 a^2 b^2");
+    assert_eq!(u1.to_string(), "x^4 y^2");
+    assert_eq!(u2.to_string(), "3 x^2 y^4");
+
     let p1 = Polynomial {
-        units: vec![&u3_1, &u5_2],
+        units: vec![u1.clone(), u2.clone()],
     };
-    assert_eq!(p1.to_string(), "");
+    assert_eq!(p1.to_string(), "x^4 y^2 + 3 x^2 y^4");
+
+    let p2 = Polynomial {
+        units: vec![u1.clone()],
+    };
+    let p3 = Polynomial {
+        units: vec![u2.clone()],
+    };
+    let p4 = p2 + p3;
+    assert_eq!(p4.to_string(), "x^4 y^2 + 3 x^2 y^4");
 }
+
